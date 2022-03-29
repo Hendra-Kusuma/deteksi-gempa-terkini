517b2660d2dadf14b38629537a62877728638207
Hendra Kusuma <hendrakusuma.vegas@gmail.com>
2022-03-29 10:21:58 +0700

    Modularisasi Dengan Fungsi

---
+++ main.py
@@ -0,0 +1,17 @@
+"""
+Aplikasi Deteksi Gempa Terkini (Modularisasi Dengan Fungsi)
+"""
+
+
+def ekstraksi_data():
+    pass
+
+
+def tampilkan_data(result):
+    pass
+
+
+if __name__ == '__main__':
+    print('aplikasi utama')
+    result = ekstraksi_data()
+    tampilkan_data(result)
