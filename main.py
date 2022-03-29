3f244bbc26da645df257c59ef18dc396b94323d4
Hendra Kusuma <hendrakusuma.vegas@gmail.com>
2022-03-29 10:30:48 +0700

    Modularisasi Dengan Fungsi

--- main.py
+++ main.py
@@ -4,6 +4,16 @@ Aplikasi Deteksi Gempa Terkini (Modularisasi Dengan Fungsi)
 
 
 def ekstraksi_data():
+    """
+    Tanggal : 29 Maret 2022,
+    Waktu : 05:56:49 WIB
+    Magnitudo : 3.7
+    Kedalaman : 1 km
+    Lokasi : 3.85 LS - 122.83 BT
+Pusat gempa berada di laut 20 km Timur Laut Soropia, Kab. Konawe
+Dirasakan (Skala MMI): II - III Kendari, II - III Konawe
+    :return:
+    """
     pass
 
 
