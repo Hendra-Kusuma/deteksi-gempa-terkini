a6da0837dfb6ab424ecfb4c69b36829ba3ed23e8
Hendra Kusuma <hendrakusuma.vegas@gmail.com>
2022-03-31 12:14:52 +0700

    Modularisasi Dengan Fungsi

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 """
 Aplikasi Deteksi Gempa Terkini (Modularisasi Dengan Fungsi)
+MODULARISASI DENGAN FUNCTION
 """
 
 
@@ -18,7 +19,7 @@ Dirasakan: Dirasakan (Skala MMI): II - III Kendari, II - III Konawe
     hasil['tanggal'] = '29 Maret 2022'
     hasil['waktu'] = '05:56:49 WIB'
     hasil['magnitudo'] = 3.7
-    hasil['lokasi'] = {'LS': 3.85, 'BT': 122.83}
+    hasil['lokasi'] = {'ls': 3.85, 'bt': 122.83}
     hasil['pusat gempa'] = 'Pusat gempa berada di laut 20 km Timur Laut Soropia, Kab. Konawe'
     hasil['dirasakan'] = 'Dirasakan (Skala MMI): II - III Kendari, II - III Konawe'
 
@@ -27,6 +28,11 @@ Dirasakan: Dirasakan (Skala MMI): II - III Kendari, II - III Konawe
 
 def tampilkan_data(result):
     print('Gempa Terakhir Berdasarkan BMKG')
+    print(f"Tanggal {result['tanggal']}")
+    print(f"Magnitudo {result['magnitudo']}")
+    print(f"Lokasi LS ={result['lokasi']['ls']}, BT = {result['lokasi']['bt']}")
+    print(f"Pusat gempa {result['pusat gempa']}")
+    print(f"Dirasakan {result['dirasakan']}")
 
 
 if __name__ == '__main__':
